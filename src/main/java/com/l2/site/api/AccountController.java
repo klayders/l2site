8771eac6c2b2667a69c3254d3082f41453cbbf4c
jura <yk@finch-melrose.com>
2019-10-18 01:51:30 +0300

    add template for user

--- src/main/java/com/l2/site/api/AccountController.java
+++ src/main/java/com/l2/site/api/AccountController.java
@@ -1,9 +1,12 @@
 package com.l2.site.api;
 
 import com.l2.site.account.AccountService;
+import com.l2.site.model.account.AccountSnapshot;
+import com.l2.site.model.account.Accounts;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.l2.site.utils.SiteUtils.API;
 
@@ -13,33 +16,33 @@ import static com.l2.site.utils.SiteUtils.API;
 public class AccountController {
 
   private static final String VERSION = "/v1";
-  static final String ACCOUNT_MAPPING = API + VERSION + "/account";
+  static final String ACCOUNT_MAPPING = API + VERSION + "/character";
 
   private final AccountService accountService;
 
-//  @GetMapping
-//  public List<AccountSnapshot> findAll() {
-//    return accountService.findAll();
-//  }
+  @GetMapping
+  public List<AccountSnapshot> findAll() {
+    return accountService.findAll();
+  }
 
-//  @GetMapping("/{login}")
-//  public AccountSnapshot getByLogin(@PathVariable String login) {
-//    return accountService.getAccountById(login);
-//  }
+  @GetMapping("/{login}")
+  public AccountSnapshot getByLogin(@PathVariable String login) {
+    return accountService.getAccountById(login);
+  }
 
-//  @PostMapping
-//  public AccountSnapshot doIt(@RequestParam String login, @RequestParam String password) {
-//    var accounts = new Accounts();
-//    accounts.setLogin(login);
-//    accounts.setPassword(password);
-//    return accountService.save(accounts);
-//  }
-//
-//  @DeleteMapping
-//  public AccountSnapshot doIt(@RequestParam String login, @RequestParam String password) {
-//    var accounts = new Accounts();
-//    accounts.setLogin(login);
-//    accounts.setPassword(password);
-//    return accountService.save(accounts);
-//  }
+  @PostMapping
+  public AccountSnapshot doIt(@RequestParam String login, @RequestParam String password) {
+    var accounts = new Accounts();
+    accounts.setLogin(login);
+    accounts.setPassword(password);
+    return accountService.save(accounts);
+  }
+
+  @DeleteMapping
+  public AccountSnapshot ss(@RequestParam String login, @RequestParam String password) {
+    var accounts = new Accounts();
+    accounts.setLogin(login);
+    accounts.setPassword(password);
+    return accountService.save(accounts);
+  }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,13 @@
-var siteApi = Vue.resource()
+var character_url = 'api/v1/character{/id}';
+var characterApi = Vue.resource(character_url);
 
 Vue.component('user-row', {
   props: ['user'],
   template:
-    '<div> <i>({{user.lastactive}})</i>' +
-    '{{user.name}}' +
+    '<div>' +
+    ' <i>({{user.id}})</i>' +
+    '{{user.charName}}' + '  ' +
+    '{{user.title}}' +
     '</div>'
 });
 
@@ -13,8 +16,16 @@ Vue.component('user-list', {
   props: ['users'],
   template:
     '<div>' +
-    '<user-row v-for="user in users" :key="user.name" :user="user"/>' +
-    '</div>'
+    '<user-row v-for="user in users" :key="user.charName" :user="user"/>' +
+    '</div>',
+  created: function () {
+    characterApi.get().then(result = >
+    result.json().then(data = > {
+      data.forEach(user = > this.users.push(user)
+  )
+  })
+  )
+  }
 });
 
 var apps = new Vue({
@@ -22,9 +33,9 @@ var apps = new Vue({
   template: '<user-list :users="users"/>',
   data: {
     users: [
-      {name: "vasya", lastactive: 132},
-      {name: "petya", lastactive: 321},
-      {name: "nastya", lastactive: 777}
+      // {name: "vasya", lastactive: 132},
+      // {name: "petya", lastactive: 321},
+      // {name: "nastya", lastactive: 777}
     ]
   }
 });
