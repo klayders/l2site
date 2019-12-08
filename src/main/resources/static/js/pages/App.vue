36a6e1397c9e5346fb3936d93fa98542fd149948
jura <yk@finch-melrose.com>
2019-12-08 14:38:48 +0300

    change background color

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <v-app style="background: url('https://i.artfile.me/wallpaper/18-05-2019/2560x1600/video-igry-heavenly-sword-mech-devushka--1459272.jpg') no-repeat center center fixed;
-  -moz-background-size: 100%;
-  -webkit-background-size: 100%;
-  -o-background-size: 100%;
-  background-size: 100%;">
+  <v-app
+    style="background: url('https://i.artfile.me/wallpaper/18-05-2019/2560x1600/video-igry-heavenly-sword-mech-devushka--1459272.jpg') no-repeat center center fixed;
+  background-size: cover;
+">
 
     <menu-bar></menu-bar>
 
@@ -30,18 +29,6 @@
     }
 </script>
 
-<style scoped>
-  /*background: url('https://i.yapx.ru/EdgG6.gif') no-repeat center center fixed;*/
-  /*-moz-background-size: 100%;*/
-  /*-webkit-background-size: 100%;*/
-  /*-o-background-size: 100%;*/
-  /*background-size: 100%;*/
+<style>
 
-  /*main {*/
-  /*  background: url('https://look.com.ua/pic/201209/1680x1050/look.com.ua-17669.jpg'); !*no-repeat center center fixed;*!*/
-  /*  -moz-background-size: 100%; !* Firefox 3.6+ *!*/
-  /*  -webkit-background-size: 100%; !* Safari 3.1+ и Chrome 4.0+ *!*/
-  /*  -o-background-size: 100%; !* Opera 9.6+ *!*/
-  /*  background-size: 100%; !* Современные браузеры *!*/
-  /*}*/
 </style>
