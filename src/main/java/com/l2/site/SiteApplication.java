3e867ba3db1e2bde062ed4b2ad28743b37798b7b
jura <yk@finch-melrose.com>
2019-12-29 01:00:29 +0300

    add verify for database

--- src/main/java/com/l2/site/SiteApplication.java
+++ src/main/java/com/l2/site/SiteApplication.java
@@ -8,7 +8,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, RepositoryRestMvcAutoConfiguration.class})
 public class SiteApplication {
 
-	public static void main(String[] args) {
+
+  public static void main(String[] args) {
 		SpringApplication.run(SiteApplication.class, args);
 	}
 
